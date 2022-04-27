0fb3db2871452575bea068ff1279a197d2ba6228
Dawid Glapiak <dg18@live.it>
2022-04-27 14:18:08 +0200

    6. styling services ection

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,6 +1,5 @@
 import home1 from '../img/home1.png';
-// STYLED 
-import styled from 'styled-components';
+import { About, Description, Hide, Image } from '../styles';
 
 const AboutSection = () => {
     return(
@@ -27,42 +26,6 @@ const AboutSection = () => {
         </About>
     )
 };
-
-// STYLED COMPONENTs
-
-const About = styled.div`
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 5rem 10rem;
-  color:white;
-`;
-
-const Description = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-  h2{
-      font-wieght: lighter;
-
-  }
-`;
-
-const Image = styled.div`
-  flex: 1;
-  overflow: hidden;
-  img{
-      width: 100%;
-      height: 80vh;
-      object-fit: cover;
-  }
-`;
-
-const Hide = styled.div`
-  overflow: hidden;
-
-`
-
 
 
 
