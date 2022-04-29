f315082320452dd8f42de947f2e5b389ccb68a2d
Dawid Glapiak <dg18@live.it>
2022-04-29 12:34:45 +0200

    about animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,20 +1,10 @@
 import home1 from '../img/home1.png';
 import { About, Description, Hide, Image } from '../styles';
 import { motion } from 'framer-motion';
+import { titleAnim, fade, photoAnim} from "../animation"
+
 
 const AboutSection = () => {
-
-    // IS BETTER TO STORE AN ANIMATION IN A VARIABLE 
-
-    // const titleAnim =  {
-    //     hidden: {opacity: 0},
-    //     show: {opacity: 1, transition: {duration: 2}},
-    // }
-
-    // const container = {
-    //     hidden: {x: 100},
-    //     show: {x: 0, transition: {duration: 1, ease: "easeOut", staggerChildren: 1}}
-    // }
 
     return(
         // THE ABOUT HERE IS A STYLED COMPONENT. I CAN CALL THE VARIABLE StyledAbout IN THE FUTURE 
@@ -22,22 +12,24 @@ const AboutSection = () => {
             <Description>
                 <motion.div >
                     <Hide>
-                        <motion.h2  >
+                        <motion.h2  variants={titleAnim} >
                              We work to make
                         </motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2 >your <span>dreams</span> come </motion.h2>
+                        <motion.h2 variants={titleAnim} >your <span>dreams</span> come </motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2 >true.</motion.h2>
+                        <motion.h2 variants={titleAnim}>true.</motion.h2>
                     </Hide>
                 </motion.div>
-                <p>contact us for any photography or videography idea that you have. We have professionals with amazing skills.</p>
-                <button>Contact us</button>
+                <motion.p variants={fade}>
+                    contact us for any photography or videography idea that you have. We have professionals with amazing skills.
+                </motion.p>
+                <motion.button variants={fade}>Contact us</motion.button>
             </Description>
             <Image>
-                <img src={home1} alt="home cover img" />
+                <motion.img variants={photoAnim} src={home1} alt="home cover img" />
             </Image>
         </About>
     )
