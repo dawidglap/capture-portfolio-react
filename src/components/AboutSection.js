c0274dd2fc7c9e23f2b659c2f7f3170deb8a55ee
Dawid Glapiak <dg18@live.it>
2022-04-29 11:18:34 +0200

    framer motion

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,22 +1,38 @@
 import home1 from '../img/home1.png';
 import { About, Description, Hide, Image } from '../styles';
+import { motion } from 'framer-motion';
 
 const AboutSection = () => {
+
+    // IS BETTER TO STORE AN ANIMATION IN A VARIABLE 
+
+    // const titleAnim =  {
+    //     hidden: {opacity: 0},
+    //     show: {opacity: 1, transition: {duration: 2}},
+    // }
+
+    // const container = {
+    //     hidden: {x: 100},
+    //     show: {x: 0, transition: {duration: 1, ease: "easeOut", staggerChildren: 1}}
+    // }
+
     return(
         // THE ABOUT HERE IS A STYLED COMPONENT. I CAN CALL THE VARIABLE StyledAbout IN THE FUTURE 
         <About> 
             <Description>
-                <div className="title">
+                <motion.div >
                     <Hide>
-                        <h2>We work to make</h2>
+                        <motion.h2  >
+                             We work to make
+                        </motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>your <span>dreams</span> come </h2>
+                        <motion.h2 >your <span>dreams</span> come </motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>true.</h2>
+                        <motion.h2 >true.</motion.h2>
                     </Hide>
-                </div>
+                </motion.div>
                 <p>contact us for any photography or videography idea that you have. We have professionals with amazing skills.</p>
                 <button>Contact us</button>
             </Description>
