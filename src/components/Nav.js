076f5ffd35e2020d4b1d1566961dc16b4dac3f4f
Dawid Glapiak <dg18@live.it>
2022-04-30 12:58:11 +0200

    project finished

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,29 @@
 import styled from "styled-components";
 import {Link} from 'react-router-dom';
+import { motion } from "framer-motion";
+import { useLocation } from "react-router";
 
 const Nav = () => {
+    const {pathname} = useLocation();
     return(
         <StyledNav>
-            <h1><Link id="logo" to="/">Capture</Link></h1>
+            <h1>
+                <Link id="logo" to="/">
+                    Capture
+                    </Link>
+            </h1>
             <ul>
                 <li>
                     <Link to="/">1. About us</Link>
+                    <Line transition={{duration: 0.75}} initial={{width: 0}} animate={{width: pathname === '/' ? '50%' : 0}}/>
                 </li>
                 <li>
                     <Link to="/work">2. Our Work</Link>
+                    <Line transition={{duration: 0.75}} initial={{width: 0}} animate={{width: pathname === '/work' ? '50%' : 0}}/>
                 </li>
                 <li>
                     <Link to="/contact">3. Contact us</Link>
+                    <Line transition={{duration: 0.75}} initial={{width: 0}} animate={{width: pathname === '/contact' ? '50%' : 0}}/>
                 </li>
             </ul>
         </StyledNav>
@@ -67,6 +77,18 @@ const StyledNav = styled.nav`
            padding: 0;
        }
    }
+`
+
+const Line = styled(motion.div)`
+  height: 0.3rem;
+  background: #23d997;
+  width: 0%;
+  position: absolute;
+  bottom: -80%;
+  left: 60%;
+  @media (max-width:1300px){
+      left: 0%;
+  }
 `
 
 export default Nav
